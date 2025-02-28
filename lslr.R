64ea627def29b47ffef5f2d2569d1bb2cb2a2dbd
janny801 <jred8069@gmail.com>
2025-02-28 15:25:24 -0600

    update code to show best fit line , make sure it display both plots

--- lslr.R
+++ lslr.R
@@ -1,9 +1,23 @@
-x= c(1,2,3,4,5,6)
-y= c(15,19,18,31,18,31) 
+# Define the data points
+x <- c(1,2,3,4,5,6)
+y <- c(15,19,18,31,18,31)
 
-model <- lm(y~x)
+# Create a linear regression model
+model <- lm(y ~ x)
 
+# Extract residuals and fitted values
 residuals <- resid(model)
-fitted_values <- fitted(model) 
+fitted_values <- fitted(model)
 
-plot(x, residuals, main = "residual plot" , xlab= "x values" , ylab = "residuals")
+# Set up a 1-row, 2-column plotting layout
+par(mfrow = c(1, 2))
+
+# Plot the original scatter plot with regression line
+plot(x, y, main = "Scatter Plot with LSRL", xlab = "X Values", ylab = "Y Values", pch = 16, col = "blue")
+abline(model, col = "red", lwd = 2)  # Add the least squares regression line
+
+# Create the residual plot without a horizontal line
+plot(x, residuals, main = "Residual Plot", xlab = "X Values", ylab = "Residuals", pch = 16, col = "blue")
+
+# Reset the plotting layout to default
+par(mfrow = c(1, 1))
